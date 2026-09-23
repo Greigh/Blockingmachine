ef843f0b1931d5f9a0e9533a330b6b552104f83e
Greigh <dghipskind@gmail.com>
2026-09-23 13:17:24 -0400

    fix(electron): disable eval devtool in main webpack config to prevent preload EvalError

--- packages/electron-app/webpack.main.config.cjs
+++ packages/electron-app/webpack.main.config.cjs
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('webpack').Configuration} */
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: false,
   entry: {
     index: './src/index.ts',
     preload: './src/preload.ts'
